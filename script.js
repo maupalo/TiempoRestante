0da0697293cd624d21d0127b7d17ca2455464f1d
Mauricio Palomares <mau.palo2@gmail.com>
2023-09-05 09:18:48 -0600

    Subir archivos

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+    const targetDate = new Date("December 1, 2023 13:30:00").getTime();
+    const currentDate = new Date().getTime();
+    const timeDifference = targetDate - currentDate;
+
+    const months = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30));
+    const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("months").textContent = months.toString().padStart(2, '0');
+    document.getElementById("days").textContent = days.toString().padStart(2, '0');
+    document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
+}
+
+// Initial call to update countdown
+updateCountdown();
+
+// Update countdown every second
+setInterval(updateCountdown, 1000);
