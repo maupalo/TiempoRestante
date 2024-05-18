91cf62463ed74768951d3c3ce78106e9b321bece
Mauricio Palomares Herrera <ingmauriciopherrera@outlook.com>
2024-05-18 12:29:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function updateCountdown() {
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    document.getElementById("months").textContent = months.toString().padStart(2, '0');
+    //document.getElementById("months").textContent = months.toString().padStart(2, '0');
     document.getElementById("days").textContent = days.toString().padStart(2, '0');
     document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
     document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
